cebb87e56c2c0c6826f55e189de485006ccd3913
dicksonmutwiriguantai <dickson.mutwiri@student.moringaschool.com>
2023-04-01 13:55:28 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+function divide(){
+let a =2000;
+let b = 100;
+    return a/b;
+
+}
+
+const square = c => c**c;//square arrow function takes one parameter and multiplies it times itself
+
+const add = (d,e)=> d+e ;//add arrow function takes two parameters and adds them together
